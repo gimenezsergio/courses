a5c565127efa87fad3f21658ac709f5c3fee23ca
sergio gimenez <gimenezsergio123@gmail.com>
2021-12-06 13:55:49 -0300

    Menu

--- js/script.js
+++ js/script.js
@@ -94,3 +94,31 @@
 	});
 
 })(jQuery);
+
+let menu = () => {
+    let template = /*html*/`
+        <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
+        <button class="navbar-toggler rounded-0" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navigation">
+          <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item @@about">
+              <a class="nav-link" href="full-stack-i.html"> Full Stack I</a>
+            </li>
+            <li class="nav-item @@courses">
+              <a class="nav-link" href="full-stack-ii.html">Full Stack II</a>
+            </li>
+
+            <li class="nav-item @@blog">
+              <a class="nav-link" href="diseno-web-de-interfaces.html">Diseño web</a>
+            </li>
+          </ul>
+        </div>
+      `
+      document.querySelector(".container nav"). innerHTML = template
+}
+menu()
